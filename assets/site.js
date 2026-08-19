30718774adccf2296b3ea6fd2e0032b90864f72c
BreakerBlock <utkarsh.khurana11@gmail.com>
2026-08-19 17:49:00 +0530

    Update site.js

--- assets/site.js
+++ assets/site.js
@@ -11,7 +11,7 @@
     LIVE: true,
     NATIVE_CONTAINER: "container-e652af360a94c45808f52efc91ed70ff",
     NATIVE_SRC: "https://pl30922225.effectivecpmnetwork.com/e652af360a94c45808f52efc91ed70ff/invoke.js",
-    SOCIALBAR_SRC: "https://pl30922223.effectivecpmnetwork.com/4d/2f/65/4d2f65c0f82ff58f590b40a96cd29098.js",
+    SOCIALBAR_SRC: "",
     SMARTLINK: ""
   };
   // expose smartlink for buttons/links across the site
